e960a5b1313ff09ef92ffbb0f4c87beed1741256
Malsaeed276 <mohamed.elsaeed276@gmail.com>
2021-09-14 09:16:46 +0300

    upload the files

---
+++ lib/models/organization.dart
@@ -0,0 +1,68 @@
+class Organization {
+  final String? title, description, link,organizationName;
+  final int? numberAttendance;
+
+  Organization({this.title, this.description, this.link,this.numberAttendance,this.organizationName});
+}
+
+List<Organization> organizationList = [
+
+  Organization(
+    title: "Google I/O Extended For Students",
+    organizationName: "Google Developer student clubs",
+description: "I was the founder and the cre host of I/O Extended for GDSC's clubs also the first time ",
+    numberAttendance: 57,
+    link: "https://www.youtube.com/watch?v=fqrh6aLGKrI",
+  ),
+  Organization(
+    title: "Hello World",
+    organizationName: "Üsküdar University Artificial Intelligence and Application Development Club",
+    description: "A conference about introduction to computer science and explain how the computer can understand the 0 and 1. also intro to algorithm",
+    numberAttendance: 26,
+    link: "https://www.instagram.com/p/B5-KlWCgyUE/",
+  ),
+  Organization(
+    title: "Info Session",
+    organizationName: "Google Developer student clubs",
+    description: "An introduction to Google Developer student club at Üsküdar Univeristy and explain what is the aim of this club and what are we going to do.",
+    numberAttendance: 36,
+    link: "https://www.youtube.com/watch?v=TxJ5f_zNzHM",
+  ),
+  Organization(
+    title: "Info Session Solution challenge",
+    organizationName: "Google Developer student clubs",
+    description: "An introduction to Solution challenge The 2021 Solution Challenge mission is to solve for one or more of the United Nations 17 Sustainable Development Goals using Google technology. That challenge will be between all GDSC (+1000 club) clubs around the World.",
+    numberAttendance: 56,
+    link: "https://www.youtube.com/watch?v=c9VoqGF0mVQ&t=3s",
+  ),
+
+  Organization(
+    title: "Flutter Workshop",
+    organizationName: "Google Developer student clubs",
+    description:"3 days workshop to introduce flutter with simple way first day was intro, 2nd say was build a simple UI, 3rd day was building something complex.",
+    numberAttendance: 20,
+    link: "https://youtube.com/playlist?list=PLZHLgRxP5G102nqngXTXVnZ0BqjbGSMsP",
+  ),
+  Organization(
+    title: "Flutter Workshop",
+    organizationName: "Google Developer student clubs",
+    description:"3 days workshop to introduce flutter with simple way first day was intro, 2nd say was build a simple UI, 3rd day was building something complex.",
+    numberAttendance: 20,
+    link: "https://youtube.com/playlist?list=PLZHLgRxP5G102nqngXTXVnZ0BqjbGSMsP",
+  ),
+  Organization(
+    title: "Hello World",
+    organizationName: "Google Developer student clubs",
+    description: "Again the same event as before but this time wirth a new updates. Hello World is a conference about introduction to computer science and explain how the computer can understand the 0 and 1. also intro to algorithm",
+    numberAttendance: 36,
+    link: "https://www.youtube.com/watch?v=a3JDnNu4LxE",
+  ),
+  Organization(
+    title: "What is GDSC",
+    organizationName: "Google Developer student clubs",
+    description: "A live talk with the leader of GDSC club at uskudar Uiveristy (whcih is me) adn also Medipol University leader and let people know more about the club.",
+    numberAttendance: 57,
+    link: "https://www.youtube.com/watch?v=fqrh6aLGKrI",
+  ),
+
+];
